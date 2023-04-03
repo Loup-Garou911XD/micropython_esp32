f630880447e6bfa5b3fbcb331633353d06c09d1b
Loup Garou <abhirajthakur699@gmail.com>
2023-04-03 09:59:48 +0530

    changed test file execution sequence

--- boot.py
+++ boot.py
@@ -5,11 +5,19 @@ import esp
 esp.osdebug("LOG_WARN")
 
 print("\n"*20+"bootstrapping")
+
 try:
     from bootstrap import *
     print("bootstrapping successfull")
 except Exception as err:
     print(err)
+
 import webrepl
 webrepl.start()
 
+try:
+    import test.py #for testing stuff at boot
+except ImportError:
+    pass
+except Exception as err:
+    print(err)
--- misc/led.py
+++ misc/led.py
@@ -2,11 +2,21 @@ import machine
 import time
 import _thread
 
-board_led = machine.Pin(2, machine.Pin.OUT)
+
+def get_board_led():
+    return machine.Pin(2, machine.Pin.OUT)
+
+
+def toggle(led=get_board_led()):
+    led.value(not led.value())
+
+
+def set(led=get_board_led(), _value=1):
+    led.value(_value)
 
 
 class BlinkLed():
-    def __init__(self, blink_delay: int = 300, blink_until: int = 0, led=board_led):
+    def __init__(self, blink_delay: int = 300, blink_until: int = 0, led=get_board_led()):
         self.led = led
         self.blink_delay = blink_delay
         self.blink_until = blink_until
@@ -24,11 +34,11 @@ class BlinkLed():
             for i in range(self.blink_until):
                 if self.condition:
                     self.blink(blink_delay=self.blink_delay)
-        self.__exit__("breh","breh","breh",) #breh
+        self.__exit__("breh", "breh", "breh",)  # breh
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
         self.condition = False
-        set(self.led,0)
+        set(self.led, 0)
 
     def blink(self, blink_delay=None):  # blink_delay in ms,if not passed the uses self.blink_delay
         if blink_delay == None:
@@ -38,22 +48,13 @@ class BlinkLed():
         toggle(self.led)
         time.sleep_ms(blink_delay)
 
-    
+
 class Glow:
-    def __init__(self,led=board_led):
+    def __init__(self, led=get_board_led()):
         self.led = led
 
     def __enter__(self):
-        set(self.led,0)
+        set(self.led, 0)
 
-    def __exit__(self,exc_type, exc_value, exc_traceback):
-        set(self.led,0)
-   
-
-def toggle(led=board_led):
-    led.value(not led.value())
-
-
-def set(led=board_led,_value=1):
-    led.value(_value)
-
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        set(self.led, 0)
